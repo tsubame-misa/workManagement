137401053195027b74bc95e50d3f87e0efce5f1b
Misato Watanabe <watamisa080513@gmail.com>
2022-12-30 00:51:01 +0900

    ログの削除

--- api.py
+++ api.py
@@ -6,123 +6,6 @@ from error import ApiError
 import datetime
 
 
-# def getUsers():
-#     url = 'https://tsubame.hasura.app/api/rest/work_management/user'
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-
-#     res = requests.get(url, headers=headers)
-#     data = res.json()
-
-#     if "error" in data:
-#         raise ApiError
-
-#     users = []
-#     for data in data["work"]:
-#         users.append(data["user_id"])
-
-#     return users
-
-
-# def getUserProjects(user):
-#     url = "https://tsubame.hasura.app/api/rest/work_management/user/projects"
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-#     data = json.dumps({"id": str(user)})
-#     res = requests.post(url, headers=headers, data=data)
-#     data = res.json()
-
-#     if "error" in data:
-#         print(data)
-#         raise ApiError
-
-#     projects = []
-#     for data in data["work"]:
-#         project = f'{data["project_name"]} : 合計作業時間 {elapsed_time_str(data["total_time"])}'
-#         if not data["start_time"] is None:
-#             start_time = datetime.datetime.strptime(
-#                 data["start_time"], '%Y-%m-%dT%H:%M:%S')
-#             project += f'  作業中（{formatDate(start_time)}から）'
-#         projects.append(project)
-#     str_project = '\n'.join(projects)
-
-#     return str_project
-
-
-# def getUserProject(user, project):
-#     url = "https://tsubame.hasura.app/api/rest/work_management/user/project"
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-#     data = json.dumps({"user_id": str(user), "project_name": project})
-#     res = requests.post(url, headers=headers, data=data)
-#     data = res.json()
-
-#     if "error" in data:
-#         raise ApiError
-
-#     if len(data["work"]) == 0:
-#         print(data)
-#         return None
-
-#     return({"start_time": data["work"][0]["start_time"],
-#             "total_time": data["work"][0]["total_time"]})
-
-
-# def insertWork(data):
-#     url = "https://tsubame.hasura.app/api/rest/work_management/insert/work"
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-#     data = json.dumps({"object": data})
-#     res = requests.post(url, headers=headers, data=data)
-#     data = res.json()
-#     if "error" in data:
-#         print(data)
-#         raise ApiError
-
-
-# def updateWork(user, project_name, start_time, total_time):
-#     url = "https://tsubame.hasura.app/api/rest/update/project"
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-#     data = json.dumps({"user_id": str(user), "project_name": project_name,
-#                       "start_time": start_time, "total_time": total_time})
-#     res = requests.put(url, headers=headers, data=data)
-#     data = res.json()
-#     if "error" in data:
-#         print(data)
-#         raise ApiError
-
-
-# def getUsers():
-#     url = 'https://tsubame.hasura.app/api/rest/work_management/users'
-#     headers = {
-#         "Content-Type": "application/json",
-#         "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
-#     }
-
-#     res = requests.get(url, headers=headers)
-#     data = res.json()
-
-#     if "error" in data:
-#         raise ApiError
-
-#     users = []
-#     for data in data["users"]:
-#         users.append(data["id"])
-
-#     return users
-
-
 def getUserProjects(user):
     url = "https://tsubame.hasura.app/api/rest/work_management/user/projects"
     headers = {
@@ -137,18 +20,6 @@ def getUserProjects(user):
         print(data)
         raise ApiError
 
-    # projects = []
-    # for data in data["projects"]:
-    #     # project = f'{data["project_name"]} : 合計作業時間 {elapsed_time_str(data["total_time"])}'
-    #     project = f'{data["name"]}'
-    #     # if not data["start_time"] is None:
-    #     #     start_time = datetime.datetime.strptime(
-    #     #         data["start_time"], '%Y-%m-%dT%H:%M:%S')
-    #     #     project += f'  作業中（{formatDate(start_time)}から）'
-    #     projects.append(project)
-    # str_project = '\n'.join(projects)
-
-    # return str_project
     return data["projects"]
 
 
@@ -200,7 +71,6 @@ def updateProject(id, total_seconds, working):
     if "error" in data:
         print(data)
         raise ApiError
-    print(data)
     return data["update_projects_by_pk"]
 
 
